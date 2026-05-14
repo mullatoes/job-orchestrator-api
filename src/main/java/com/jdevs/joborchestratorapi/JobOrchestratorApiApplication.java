de25bd43e296b6a4b7df81f7a46ecbc0b4aea361
kyeiiih <jaydevsengineer@gmail.com>
2026-05-14 23:37:00 +0300

    Initial commit

---
+++ src/main/java/com/jdevs/joborchestratorapi/JobOrchestratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdevs.joborchestratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOrchestratorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOrchestratorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdevs/joborchestratorapi/JobOrchestratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdevs.joborchestratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOrchestratorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
